c67bf3b9d02d86e71bd48e0e250d88596d35f668
DavidWendtNJIT <djw5383@gmail.com>
2022-04-02 16:04:18 -0400

    Plane moves in all directions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gameArea = document.querySelector(".gameArea");
 const gameMessage = document.querySelector(".gameMessage");
 
 document.addEventListener("keydown", pressOn);
-document.addEventListener("keyUp", pressOff);
+document.addEventListener("keyup", pressOff);
 document.addEventListener("click", start);
 
 let player = {
@@ -20,19 +20,44 @@ function start(){
   player.plane = document.createElement("div");
   player.plane.setAttribute("class","plane");
   gameArea.appendChild(player.plane);
+  window.requestAnimationFrame(playGame);
+  player.x = player.plane.offsetLeft;
+  player.y = player.plane.offsetTop;
+}
+
+function playGame(){
+  if(player.inplay){
+  console.log(keys)
+  if(keys.ArrowUp){
+    player.y -= player.speed;
+  }
+  if (keys.ArrowDown) {
+    player.y += player.speed;
+  }
+  if (keys.ArrowLeft) {
+    player.x -= player.speed;
+  }
+  if (keys.ArrowRight) {
+    player.x += player.speed;
+  }
+
+  player.plane.style.left = player.x + "px";
+  player.plane.style.top = player.y + "px";
+  window.requestAnimationFrame(playGame);
+  }
 }
 
 // Key Stroke Functions
 function pressOn(e) {
   e.preventDefault();
-  let tempKey = e.key == " " ? "space" : e.key;
+  let tempKey = (e.key == " ") ? "space" : e.key;
   keys[tempKey] = true;
   console.log(keys);
 }
 
 function pressOff(e) {
   e.preventDefault();
-  let tempKey = e.key == " " ? "space" : e.key;
+  let tempKey = (e.key == " ") ? "space" : e.key;
   keys[tempKey] = false;
   console.log(keys);
 }
